479c233a31dddb0710c2a75f15007c598f90453d
bilalwaheedch <bilalwaheedch@me.com>
2016-11-27 10:42:59 -0500

    initial setup

---
+++ Project1/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int number = 10;
+		for (i = 0,i < 10, i++){
+			System.out.println(i ++ " x "++ number ++ "="++ i*number);
+		}
+				
+	}
+
+}
